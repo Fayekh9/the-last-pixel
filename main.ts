7d078dce951be3f01000ed18c435beef070dd956
Fayekh9 <104355675+Fayekh9@users.noreply.github.com>
2024-01-05 15:31:49 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,307 @@
+input.onButtonPressed(Button.A, function () {
+    music.playTone(494, music.beat(BeatFraction.Quarter))
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.clearScreen()
+})
+// The game shows pixels and one by one they will be deleted!
+basic.showString("-----")
+music.setVolume(88)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
+basic.pause(100)
+music.playTone(494, music.beat(BeatFraction.Eighth))
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    . # # # #
+    # # # # #
+    # # # # #
+    `)
+basic.pause(100)
+music.playTone(494, music.beat(BeatFraction.Eighth))
+basic.showLeds(`
+    # # . # #
+    # # # # #
+    . # # # #
+    # # # # #
+    # # # # #
+    `)
+basic.pause(100)
+music.playTone(494, music.beat(BeatFraction.Eighth))
+basic.showLeds(`
+    # # . # #
+    # # # # .
+    . # # # #
+    # # # # #
+    # # # # #
+    `)
+basic.pause(100)
+music.playTone(494, music.beat(BeatFraction.Eighth))
+basic.showLeds(`
+    # # . # #
+    # # # # .
+    . # # # #
+    # # # # #
+    # # . # #
+    `)
+basic.pause(100)
+music.playTone(494, music.beat(BeatFraction.Eighth))
+basic.showLeds(`
+    # # . # #
+    # # # # .
+    . # # # #
+    # # # # #
+    . # . # #
+    `)
+basic.pause(100)
+music.playTone(494, music.beat(BeatFraction.Eighth))
+basic.showLeds(`
+    # # . # #
+    # # # # .
+    . # # # #
+    # # # # .
+    . # . # #
+    `)
+basic.pause(100)
+music.playTone(494, music.beat(BeatFraction.Eighth))
+basic.showLeds(`
+    . # . # #
+    # # # # .
+    . # # # #
+    # # # # .
+    . # . # #
+    `)
+basic.pause(100)
+music.playTone(494, music.beat(BeatFraction.Eighth))
+basic.showLeds(`
+    . # . . #
+    # # # # .
+    . # # # #
+    # # # # .
+    . # . # #
+    `)
+basic.pause(100)
+music.playTone(494, music.beat(BeatFraction.Eighth))
+basic.showLeds(`
+    . # . . #
+    # # # # .
+    . # # # #
+    # # # # .
+    . # . . #
+    `)
+basic.pause(100)
+music.playTone(494, music.beat(BeatFraction.Eighth))
+basic.showLeds(`
+    . # . . .
+    # # # # .
+    . # # # #
+    # # # # .
+    . # . . #
+    `)
+basic.pause(100)
+music.playTone(494, music.beat(BeatFraction.Eighth))
+basic.showLeds(`
+    . # . . .
+    # # # # .
+    . # # # #
+    # # # # .
+    . . . . #
+    `)
+basic.pause(100)
+music.playTone(494, music.beat(BeatFraction.Eighth))
+basic.showLeds(`
+    . . . . .
+    # # # # .
+    . # # # #
+    # # # # .
+    . . . . #
+    `)
+basic.pause(100)
+music.playTone(494, music.beat(BeatFraction.Eighth))
+basic.showLeds(`
+    . . . . .
+    # # # # .
+    . # # # #
+    # # # # .
+    . . . . .
+    `)
+basic.pause(100)
+music.playTone(494, music.beat(BeatFraction.Eighth))
+basic.showLeds(`
+    . . . . .
+    . # # # .
+    . # # # #
+    # # # # .
+    . . . . .
+    `)
+basic.pause(100)
+music.playTone(494, music.beat(BeatFraction.Eighth))
+basic.showLeds(`
+    . . . . .
+    . # # # .
+    . # # # #
+    . # # # .
+    . . . . .
+    `)
+basic.pause(100)
+music.playTone(494, music.beat(BeatFraction.Quarter))
+basic.showLeds(`
+    . . . . .
+    . # # # .
+    . # # # .
+    . # # # .
+    . . . . .
+    `)
+basic.pause(200)
+music.playTone(494, music.beat(BeatFraction.Quarter))
+basic.showLeds(`
+    . . . . .
+    . # # # .
+    . . # # .
+    . # # # .
+    . . . . .
+    `)
+basic.pause(200)
+music.playTone(494, music.beat(BeatFraction.Quarter))
+basic.showLeds(`
+    . . . . .
+    . # # # .
+    . . # # .
+    . # # . .
+    . . . . .
+    `)
+basic.pause(200)
+music.playTone(494, music.beat(BeatFraction.Quarter))
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . # # .
+    . # # . .
+    . . . . .
+    `)
+basic.pause(200)
+music.playTone(494, music.beat(BeatFraction.Quarter))
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . # # .
+    . . # . .
+    . . . . .
+    `)
+basic.pause(200)
+music.playTone(494, music.beat(BeatFraction.Quarter))
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . # . .
+    . . # . .
+    . . . . .
+    `)
+basic.pause(200)
+music.playTone(494, music.beat(BeatFraction.Quarter))
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(200)
+music.playTone(494, music.beat(BeatFraction.Quarter))
+basic.showLeds(`
+    . . . . .
+    . # . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(200)
+music.playTone(494, music.beat(BeatFraction.Quarter))
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(500)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(500)
+music.playTone(494, music.beat(BeatFraction.Quarter))
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(500)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(500)
+music.playTone(494, music.beat(BeatFraction.Quarter))
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(500)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+music.playTone(494, music.beat(BeatFraction.Quarter))
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(500)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+music.playTone(494, music.beat(BeatFraction.Double))
